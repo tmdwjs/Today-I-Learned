88760a9bb835531291568903deb6a9417a75cd21
BaikSeungJeon <tmdwjsofficial@naver.com>
2023-04-01 00:39:39 +0900

    2023 04 01 TIL 코테

---
+++ Algorithm/Baekjoon/단계별/Implementation/10718_We love kriii.java
@@ -0,0 +1,10 @@
+// https://www.acmicpc.net/problem/10718
+
+package baekjoon;
+
+public class Main14 {
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+}
---
+++ Algorithm/Baekjoon/단계별/Math/2558_A+B-2.java
@@ -0,0 +1,15 @@
+// https://www.acmicpc.net/problem/2558
+
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Main15 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		
+		System.out.println(n1 + n2);
+	}
+}
