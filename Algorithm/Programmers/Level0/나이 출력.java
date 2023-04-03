4042eecd60ede378a40f073c666a7fc3ab3b59b6
BaikSeungJeon <tmdwjsofficial@naver.com>
2023-04-03 18:44:34 +0900

    2023 04 03 월 TIL 프로그래머스

---
+++ Algorithm/Programmers/Level0/나이 출력.java
@@ -0,0 +1,13 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120820
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int age) {
+    int answer = 0;
+    
+    answer = 2022 - age + 1;
+    
+    return answer;
+}
