cda338ef056d48d23ae48c3dc22fc3f8480ce838
BaikSeungJeon <tmdwjsofficial@naver.com>
2023-04-03 18:56:03 +0900

    2023 04 03 월 TIL 프로그래머스

---
+++ Algorithm/Programmers/Level0/두 수의 나눗셈.java
@@ -0,0 +1,9 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120806
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    return ((double)num1 / (double)num2) * 1000;
+}
