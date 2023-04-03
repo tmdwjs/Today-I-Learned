514613ab31d07e937c959b2e38c9884314a2d5ef
BaikSeungJeon <tmdwjsofficial@naver.com>
2023-04-03 18:46:00 +0900

    2023 04 03 월 TIL 프로그래머스

---
+++ Algorithm/Programmers/Level0/두 수의 합.java
@@ -0,0 +1,13 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120802
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    int answer = -1;
+    
+    answer = num1 + num2;
+    
+    return answer;
+}
