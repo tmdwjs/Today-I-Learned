12df8c79960e3eec849edac2360213e491b0fb43
BaikSeungJeon <tmdwjsofficial@naver.com>
2023-04-08 01:24:54 +0900

    2023 04 08 토 TIL

---
+++ Algorithm/Programmers/Level0/팩토리얼.java
@@ -0,0 +1,24 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120848
+
+class Solution {
+  public int solution(int n) {
+      int ans = 0;
+      
+      for(int i = 1; i <= n; i++){
+          if(isFactorial(i) <= n){
+              ans = i;
+          }else {
+              break;
+          }
+      }
+      
+      return ans;
+  }
+  
+  public int isFactorial(int num) {
+      int result = 1;
+      for(int i = num; i > 1; i--) result *= i;
+      
+      return result;
+  }
+}
