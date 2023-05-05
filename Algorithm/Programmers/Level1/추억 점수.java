3d763309210260703c7d3dfdb6bd00611f5baf7f
BaikSeungJeon <tmdwjsofficial@naver.com>
2023-05-05 15:45:15 +0900

    2023 05 05 금 TIL 알고리즘 프로그래머스

---
+++ Algorithm/Programmers/Level1/추억 점수.java
@@ -0,0 +1,23 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/176963
+
+class Solution {
+  public int[] solution(String[] name, int[] yearning, String[][] photo) {
+      int[] answer = new int[photo.length];
+      
+      for (int i = 0; i < photo.length; i++) {
+          int sum = 0;
+          
+          for (int j = 0; j < photo[i].length; j++) {
+              for (int k = 0; k < name.length; k++) {
+                  if (name[k].equals(photo[i][j])) {
+                      sum += yearning[k];
+                  }
+              }
+          }
+          
+          answer[i] = sum;
+      }
+      
+      return answer;
+  }
+}
