552747f7dbbd8da08cf24c751686ba74b4294bf8
BaikSeungJeon <tmdwjsofficial@naver.com>
2023-05-10 15:37:31 +0900

    2023 05 10 수 TIL 알고리즘 프로그래머스

---
+++ Algorithm/Programmers/Level1/예산.java
@@ -0,0 +1,21 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12982?language=java
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int total = 0;
+        int team = 0;
+        
+        Arrays.sort(d);
+        
+        for (int i = 0; i < d.length; i++) {
+            total += d[i];
+            if (total > budget) break;
+            team ++;
+            
+        }
+        
+        return team;
+    }
+}
