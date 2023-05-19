d0e17772173b5db7c24e1062763dc10be30ea2c8
BaikSeungJeon <tmdwjsofficial@naver.com>
2023-05-19 16:02:19 +0900

    2023 05 19 금

---
+++ Algorithm/Programmers/Level2/모음사전.java
@@ -0,0 +1,22 @@
+package programmers.level2;
+
+public class 모음사전 {
+    public static void main(String[] args) {
+        String word = "EIO";
+        System.out.println(solution(word));
+    }
+
+    public static int solution(String word) {
+        int answer = 0;
+        int[] arr = {781, 156, 31, 6, 1};
+        String str = "AEIOU";
+        int idx = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            idx = str.indexOf(word.charAt(i));
+            answer += (arr[i] * idx) + 1;
+        }
+
+        return answer;
+    }
+}
