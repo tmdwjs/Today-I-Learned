cd26aac278593325a34528fd6162a2a13159e38a
BaikSeungJeon <tmdwjsofficial@naver.com>
2023-05-25 14:13:14 +0900

    2023 05 25 목 코테

--- Algorithm/Programmers/Level2/전화번호 목록.java
+++ Algorithm/Programmers/Level2/전화번호 목록.java
@@ -66,3 +66,38 @@ public class 전화번호_목록 {
         return answer;
     }
 }
+
+// 해시를 이용해 한 번 더 풂
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/42577
+
+// package programmers.level2;
+
+// import java.util.HashMap;
+// import java.util.Map;
+
+// public class 전화번호_목록_2 {
+//     public static void main(String[] args) {
+//         String[] phone_book = {"119", "97674223", "1195524421"};
+//         System.out.println(solution(phone_book));
+//     }
+
+//     public static boolean solution(String[] phone_book) {
+//         boolean answer = true;
+//         Map<String, Integer> map = new HashMap<>();
+
+//         for (int i = 0; i < phone_book.length; i++) {
+//             map.put(phone_book[i], i);
+//         }
+
+//         for (int i = 0; i < phone_book.length; i++) {
+//             for (int j = 0; j < phone_book[i].length(); j++) {
+//                 if (map.containsKey(phone_book[i].substring(0, j))) {
+//                     answer = false;
+//                 }
+//             }
+//         }
+
+//         return answer;
+//     }
+// }
