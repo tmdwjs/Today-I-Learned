b0e4b839de3aab7865cc5178c81def8d34ea025c
BaikSeungJeon <tmdwjsofficial@naver.com>
2023-08-07 09:52:21 +0900

    2023 08 07 코테 백준

---
+++ 알고리즘/백준/dp/1912_연속합.java
@@ -0,0 +1,35 @@
+// https://www.acmicpc.net/problem/1912
+
+package baekjoon.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 연속합_1912 {
+    public static void main(String[] args) throws IOException {
+        // 1. 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[0] = arr[0];
+        int max = dp[0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(max);
+        
+    }
+}
