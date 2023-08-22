adc06f1c307adc2a56f60a0c6a6648030253aa27
BaikSeungJeon <tmdwjsofficial@naver.com>
2023-08-22 14:30:12 +0900

    2023 08 22 화 코테 백준 연결리스트

--- 알고리즘/백준/old/Silver/1406_에디터.java
+++ 알고리즘/백준/old/Silver/1406_에디터.java
@@ -1,4 +1,4 @@
-// ListIterator 사용
+// 복습
 package baekjoon.silver;
 
 import java.io.*;
@@ -11,60 +11,46 @@ public class 에디터_1406 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String str = br.readLine(); // 문자
-        int n = Integer.parseInt(br.readLine()); // 커맨드 개수
+        String str = br.readLine();
+        int n = Integer.parseInt(br.readLine());
 
-        // 문자를 linkedList에 담기
         LinkedList<Character> list = new LinkedList<>();
-
         for (int i = 0; i < str.length(); i++) {
             list.add(str.charAt(i));
         }
 
-        // ListIterator 사용
-        // ListIterator는 양방향 탐색이 가능하다.
-        // list -> iter
         ListIterator<Character> iter = list.listIterator();
 
-        // 커서
-        // 처음 시작 시 커서는 맨 뒤에서 시작해야 함(abe|)
-        while(iter.hasNext()) {
-            iter.next(); // 맨 뒤로 보내는 과정
+        // cursor 맨 뒤 위치
+        // 다음 요소가 있는지 파악해 존재 시 맨 뒤로 위치 시키는 과정
+        while (iter.hasNext()) {
+            iter.next();
         }
 
-        // n만큼 돌며 커맨드를 한 줄씩 받음
         for (int i = 0; i < n; i++) {
             String command = br.readLine();
             char c = command.charAt(0);
 
             switch (c) {
-                case 'L':
-                    // 이전으로(왼쪽으로) 보낼 수 있으면
-                    if (iter.hasPrevious()) {
-                        iter.previous(); // previous 실행
-                    }
-
-                    break;
-
-                case 'D':
-                    if (iter.hasNext()) {
-                        iter.next(); // previous 실행
-                    }
-
-                    break;
-
-                case 'B':
+                case 'L' :
                     if (iter.hasPrevious()) {
                         iter.previous();
-                        iter.remove();;
                     }
-
                     break;
-
+                case 'D' :
+                    if (iter.hasNext()) {
+                        iter.next();
+                    }
+                    break;
+                case 'B' :
+                    if (iter.hasPrevious()) {
+                        iter.previous();
+                        iter.remove();
+                    }
+                    break;
                 default:
                     char s = command.charAt(2);
                     iter.add(s);
-
                     break;
             }
         }
@@ -77,6 +63,86 @@ public class 에디터_1406 {
         bw.close();
     }
 }
+
+// ListIterator 사용
+// package baekjoon.silver;
+
+// import java.io.*;
+// import java.util.LinkedList;
+// import java.util.ListIterator;
+// import java.util.StringTokenizer;
+
+// public class 에디터_1406 {
+//     public static void main(String[] args) throws IOException {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+//         String str = br.readLine(); // 문자
+//         int n = Integer.parseInt(br.readLine()); // 커맨드 개수
+
+//         // 문자를 linkedList에 담기
+//         LinkedList<Character> list = new LinkedList<>();
+
+//         for (int i = 0; i < str.length(); i++) {
+//             list.add(str.charAt(i));
+//         }
+
+//         // ListIterator 사용
+//         // ListIterator는 양방향 탐색이 가능하다.
+//         // list -> iter
+//         ListIterator<Character> iter = list.listIterator();
+
+//         // 커서
+//         // 처음 시작 시 커서는 맨 뒤에서 시작해야 함(abe|)
+//         while(iter.hasNext()) {
+//             iter.next(); // 맨 뒤로 보내는 과정
+//         }
+
+//         // n만큼 돌며 커맨드를 한 줄씩 받음
+//         for (int i = 0; i < n; i++) {
+//             String command = br.readLine();
+//             char c = command.charAt(0);
+
+//             switch (c) {
+//                 case 'L':
+//                     // 이전으로(왼쪽으로) 보낼 수 있으면
+//                     if (iter.hasPrevious()) {
+//                         iter.previous(); // previous 실행
+//                     }
+
+//                     break;
+
+//                 case 'D':
+//                     if (iter.hasNext()) {
+//                         iter.next(); // previous 실행
+//                     }
+
+//                     break;
+
+//                 case 'B':
+//                     if (iter.hasPrevious()) {
+//                         iter.previous();
+//                         iter.remove();;
+//                     }
+
+//                     break;
+
+//                 default:
+//                     char s = command.charAt(2);
+//                     iter.add(s);
+
+//                     break;
+//             }
+//         }
+
+//         for (char el: list) {
+//             bw.write(el);
+//         }
+
+//         bw.flush();
+//         bw.close();
+//     }
+// }
 
 // LinkedList 사용
 // 시간 초과
