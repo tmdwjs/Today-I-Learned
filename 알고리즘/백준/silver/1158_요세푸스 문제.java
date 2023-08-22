9fac6a065e9f94609753d256e1caa3d09d05a8b4
BaikSeungJeon <tmdwjsofficial@naver.com>
2023-08-22 17:55:34 +0900

    2023 08 22 화 코테

---
+++ 알고리즘/백준/silver/1158_요세푸스 문제.java
@@ -0,0 +1,46 @@
+package baekjoon.linkedlist;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class 요세푸스_문제_1158 {
+    public static void main(String[] args) throws IOException {
+        // 1. 입출력 받기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 1; i <= n; i++) {
+            q.offer(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+
+        while(!q.isEmpty()) {
+            for (int i = 0; i < k - 1; i++) {
+                q.offer(q.poll());
+            }
+
+            if (q.size() == 1) {
+                sb.append(q.poll());
+            } else {
+                sb.append(q.poll() + ", ");
+            }
+        }
+
+        sb.append(">");
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
