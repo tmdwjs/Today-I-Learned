7029d79fe42148ab43a202f028df5cf82a55d20e
BaikSeungJeon <tmdwjsofficial@naver.com>
2023-08-23 13:28:29 +0900

    2023 08 23 화 코테

--- 알고리즘/백준/silver/1021_회전하는 큐.java
+++ 알고리즘/백준/silver/1021_회전하는 큐.java
@@ -1,66 +1,121 @@
+// 복습
 // https://www.acmicpc.net/problem/1021
 
 package baekjoon.silver;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class 회전하는_큐_1021 {
     public static void main(String[] args) throws IOException {
-        // 1. 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken()); // 큐의 크기
-        int m = Integer.parseInt(st.nextToken()); // 뽑고자 하는 개수
-        LinkedList<Integer> deque = new LinkedList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        // 1부터 주어진 n까지 덱에 담음
+        LinkedList<Integer> deq = new LinkedList<>();
         for (int i = 1; i <= n; i++) {
-            deque.offer(i);
+            deq.offer(i); // n = 10, {1, 2, ..., 9, 10}
         }
 
-        int[] arr = new int[m]; // 뽑고자 하는 배열
+        int[] arr = new int[m];
         st = new StringTokenizer(br.readLine());
-
         for (int i = 0; i < m; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int count = 0; // 카운트 할 변수
+        int cnt = 0;
 
-        // 2. 로직
         for (int i = 0; i < m; i++) {
-            int target_idx = deque.indexOf(arr[i]); // 찾고자 하는 요소의 인덱스 타겟으로 잡음
-            int half_idx = deque.size() % 2 == 0 ? deque.size() / 2 - 1 : deque.size() / 2;
+            int target_idx = deq.indexOf(arr[i]);
+            int half_idx = deq.size() % 2 == 0 ? deq.size() / 2 - 1 : deq.size() / 2;
 
-            // 타겟 인덱스가 중간보다 왼쪽이면, 왼쪽으로 보내기 = 2번 연산 실행
+            // 찾으려는 숫자의 인덱스가, 중앙보다 왼쪽이면, 왼쪽으로 회전
             if (target_idx <= half_idx) {
-                // 타겟 인덱스만큼 연산 실행
                 for (int j = 0; j < target_idx; j++) {
-                    int temp = deque.pollFirst();
-                    deque.offerLast(temp);
-                    count ++;
+                    deq.offerLast(deq.pollFirst());
+                    cnt++;
                 }
-
-            // 타겟 인덱스가 중간보다 오른쪽이면, 오른쪽으로 보내기 = 3번 연산 실행
+            // 찾으려는 숫자의 인덱스가, 중앙보다 오른쪽이면, 오른쪽으로 회전
             } else {
-                // 타겟 인덱스만큼 연산 실행
-                for (int j = 0; j < deque.size() - target_idx; j++) {
-                    int temp = deque.pollLast();
-                    deque.offerFirst(temp);
-                    count ++;
+                for (int j = 0; j < deq.size() - target_idx; j++) {
+                    deq.offerFirst(deq.pollLast());
+                    cnt++;
                 }
             }
 
-            deque.pollFirst(); // 연산 종료 시 맨 앞 원소 삭제 = 1번 연산 실행
-
+            deq.pollFirst();
         }
 
-        System.out.println(count);
-
+        System.out.println(cnt);
     }
 }
+
+
+// // https://www.acmicpc.net/problem/1021
+
+// package baekjoon.silver;
+
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.util.LinkedList;
+// import java.util.StringTokenizer;
+
+// public class 회전하는_큐_1021 {
+//     public static void main(String[] args) throws IOException {
+//         // 1. 입력
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         StringTokenizer st;
+//         st = new StringTokenizer(br.readLine());
+//         int n = Integer.parseInt(st.nextToken()); // 큐의 크기
+//         int m = Integer.parseInt(st.nextToken()); // 뽑고자 하는 개수
+//         LinkedList<Integer> deque = new LinkedList<>();
+
+//         // 1부터 주어진 n까지 덱에 담음
+//         for (int i = 1; i <= n; i++) {
+//             deque.offer(i);
+//         }
+
+//         int[] arr = new int[m]; // 뽑고자 하는 배열
+//         st = new StringTokenizer(br.readLine());
+
+//         for (int i = 0; i < m; i++) {
+//             arr[i] = Integer.parseInt(st.nextToken());
+//         }
+
+//         int count = 0; // 카운트 할 변수
+
+//         // 2. 로직
+//         for (int i = 0; i < m; i++) {
+//             int target_idx = deque.indexOf(arr[i]); // 찾고자 하는 요소의 인덱스 타겟으로 잡음
+//             int half_idx = deque.size() % 2 == 0 ? deque.size() / 2 - 1 : deque.size() / 2;
+
+//             // 타겟 인덱스가 중간보다 왼쪽이면, 왼쪽으로 보내기 = 2번 연산 실행
+//             if (target_idx <= half_idx) {
+//                 // 타겟 인덱스만큼 연산 실행
+//                 for (int j = 0; j < target_idx; j++) {
+//                     int temp = deque.pollFirst();
+//                     deque.offerLast(temp);
+//                     count ++;
+//                 }
+
+//             // 타겟 인덱스가 중간보다 오른쪽이면, 오른쪽으로 보내기 = 3번 연산 실행
+//             } else {
+//                 // 타겟 인덱스만큼 연산 실행
+//                 for (int j = 0; j < deque.size() - target_idx; j++) {
+//                     int temp = deque.pollLast();
+//                     deque.offerFirst(temp);
+//                     count ++;
+//                 }
+//             }
+
+//             deque.pollFirst(); // 연산 종료 시 맨 앞 원소 삭제 = 1번 연산 실행
+
+//         }
+
+//         System.out.println(count);
+
+//     }
+// }
