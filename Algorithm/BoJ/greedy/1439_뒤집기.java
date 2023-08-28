1fc206c656c374b912eb7ca1df2e2b9e3b1d41a6
BaikSeungJeon <tmdwjsofficial@naver.com>
2023-08-28 10:00:44 +0900

    2023 08 28 월 코테

---
+++ Algorithm/BoJ/greedy/1439_뒤집기.java
@@ -0,0 +1,17 @@
+package baekjoon.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 뒤집기_1439 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        StringTokenizer st1 = new StringTokenizer(s, "0"); // 0으로 split 하니 토큰 1의 개수를 구할 수 있음
+        StringTokenizer st0 = new StringTokenizer(s, "1"); // 1로 split 하니 토큰 0의 개수를 구할 수 있음
+
+        System.out.println(Math.min(st1.countTokens(), st0.countTokens()));
+    }
+}
