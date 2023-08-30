e9c0af7e127536ffa2a9f10a134242c0b3ce8bb0
BaikSeungJeon <tmdwjsofficial@naver.com>
2023-08-30 13:39:09 +0900

    2023 08 30 수 코테

---
+++ Algorithm/BoJ/silver/11279_최대 합.java
@@ -0,0 +1,29 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class 최대_합_11279 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (x > 0) {
+                pq.add(x);
+            } else {
+                if (pq.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(pq.poll());
+                }
+            }
+        }
+    }
+}
