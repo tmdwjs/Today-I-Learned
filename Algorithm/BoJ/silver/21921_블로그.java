a4319ff8538052f6b64d4b1bce2b428fd8087651
백승전 <85447054+tmdwjs@users.noreply.github.com>
2023-09-12 16:16:34 +0900

    2023 09 12 화 코테

--- Algorithm/BoJ/silver/21921_블로그.java
+++ Algorithm/BoJ/silver/21921_블로그.java
@@ -1,6 +1,66 @@
+// 시도 2. 통과
 // https://www.acmicpc.net/problem/21921
 
-// 시도 2. 통과
+package silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class 블로그_21921 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+        int[] today = new int[n];
+        int check = 0;
+        int max = 0;
+        int cnt = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int el = Integer.parseInt(st.nextToken());
+            today[i] = el;
+            check += el;
+        }
+
+        if (check == 0) {
+            System.out.println("SAD");
+        } else {
+            int sum = 0;
+
+            for (int i = 0; i < x; i++) {
+                sum += today[i];
+            }
+
+            max = sum;
+            cnt = 1;
+
+            for (int i = x; i < n; i++) {
+                sum = sum - today[i - x] + today[i];
+
+                if (sum > max) {
+                    max = sum;
+                    cnt = 1;
+                } else if (sum == max){
+                    cnt ++;
+                }
+            }
+
+            System.out.println(max);
+            System.out.println(cnt);
+        }
+    }
+}
+
+
+// 시도 1. 시간 초과
+// https://www.acmicpc.net/problem/21921
+
 package silver;
 
 import java.io.BufferedReader;
@@ -52,57 +112,3 @@ public class 블로그_21921 {
         }
     }
 }
-
-
-// 시도 1. 시간 초과
-// package silver;
-
-// import java.io.BufferedReader;
-// import java.io.IOException;
-// import java.io.InputStreamReader;
-// import java.util.StringTokenizer;
-
-// public class 블로그_21921 {
-//     public static void main(String[] args) throws IOException {
-//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//         StringTokenizer st = new StringTokenizer(br.readLine());
-
-//         int n = Integer.parseInt(st.nextToken());
-//         int x = Integer.parseInt(st.nextToken());
-//         int[] today = new int[n];
-//         int check = 0;
-//         int max = 0;
-//         int count = 0;
-
-//         st = new StringTokenizer(br.readLine());
-//         for (int i = 0; i < n; i++) {
-//             int el = Integer.parseInt(st.nextToken());
-//             today[i] = el;
-//             check += el;
-//         }
-
-//         if (check == 0) {
-//             System.out.println("SAD");
-//         } else {
-//             int sum = 0;
-//             for (int i = 0; i < x; i++) {
-//                 sum += today[i];
-//             }
-//             max = sum;
-//             count = 1;
-
-//             for (int i = x; i < n; i++) {
-//                 sum = sum - today[i - x] + today[i];
-//                 if (sum > max) {
-//                     max = sum;
-//                     count = 1;
-//                 } else if (sum == max) {
-//                     count++;
-//                 }
-//             }
-
-//             System.out.println(max);
-//             System.out.println(count);
-//         }
-//     }
-// }
