a6aaed5797205a21906266129a4e016157b1c5fc
baikseungjeon <tmdwjsofficial@naver.com>
2023-09-13 10:28:06 +0900

    2023 09 13 수 TIL

--- Algorithm/BoJ/silver/21921_블로그.java
+++ Algorithm/BoJ/silver/21921_블로그.java
@@ -41,7 +41,7 @@ public class 블로그_21921 {
             cnt = 1;
 
             for (int i = x; i < n; i++) {
-                sum = sum - today[i - x] + today[i];
+                sum += today[i] - today[i - x];
 
                 if (sum > max) {
                     max = sum;
@@ -61,11 +61,12 @@ public class 블로그_21921 {
 // 시도 1. 시간 초과
 // https://www.acmicpc.net/problem/21921
 
-package silver;
+/* package silver;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class 블로그_21921 {
@@ -78,7 +79,7 @@ public class 블로그_21921 {
         int[] today = new int[n];
         int check = 0;
         int max = 0;
-        int count = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
@@ -90,25 +91,21 @@ public class 블로그_21921 {
         if (check == 0) {
             System.out.println("SAD");
         } else {
-            int sum = 0;
-            for (int i = 0; i < x; i++) {
-                sum += today[i];
-            }
-            max = sum;
-            count = 1;
+            for (int i = 0; i < n - x + 1; i++) {
+                int sum = 0;
 
-            for (int i = x; i < n; i++) {
-                sum = sum - today[i - x] + today[i];
-                if (sum > max) {
+                for (int j = i; j < i + x; j++) {
+                    sum += today[j];
+                }
+
+                if (sum >= max) {
                     max = sum;
-                    count = 1;
-                } else if (sum == max) {
-                    count++;
+                    map.put(max, map.getOrDefault(max, 0) + 1);
                 }
             }
 
             System.out.println(max);
-            System.out.println(count);
+            System.out.println(map.get(max));
         }
     }
-}
+} */
