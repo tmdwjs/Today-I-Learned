3af51ea8bea7cc2ac49b00df37f7f615bc456008
baikseungjeon <tmdwjsofficial@naver.com>
2023-09-19 11:24:08 +0900

    2023 09 19 화 코테

--- Algorithm/Programmers/Level1/명예의 전당 (1).java
+++ Algorithm/Programmers/Level1/명예의 전당 (1).java
@@ -21,3 +21,27 @@ class Solution {
         return ans;
     }
 }
+
+// 2회차 풀이
+
+// import java.util.*;
+
+// class Solution {
+//     public int[] solution(int k, int[] score) {
+//         int[] answer = new int[score.length];
+        
+//         PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+//         for (int i = 0; i < score.length; i++) {
+//             pq.add(score[i]);
+            
+//             if (pq.size() > k) {
+//                 pq.poll();
+//             }
+            
+//             answer[i] = pq.peek();
+//         }
+        
+//         return answer;
+//     }
+// }
