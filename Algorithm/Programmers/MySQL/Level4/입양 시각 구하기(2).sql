dddab94389d66d2fded366eb77d25dda2b0941e3
baikseungjeon <tmdwjsofficial@naver.com>
2023-10-15 18:00:39 +0900

    2023 10 15 일 SQL

---
+++ Algorithm/Programmers/MySQL/Level4/입양 시각 구하기(2).sql
@@ -0,0 +1,20 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59413
+
+WITH RECURSIVE HOURS AS (
+    SELECT 0 AS NUM
+    
+    UNION ALL
+    
+    SELECT NUM + 1
+    FROM HOURS
+    WHERE NUM < 23
+)
+
+
+SELECT H.NUM AS HOUR, IFNULL(A.COUNT, 0) AS COUNT
+FROM HOURS H
+LEFT OUTER JOIN ( SELECT HOUR(DATETIME) AS HOUR, COUNT(ANIMAL_ID) AS COUNT
+            FROM ANIMAL_OUTS
+            GROUP BY HOUR
+            ORDER BY HOUR) A
+ON H.NUM = A.HOUR
