806ceea6c6467ae8cb5d0b76212736efb80307e0
baikseungjeon <tmdwjsofficial@naver.com>
2023-10-19 13:03:01 +0900

    2023 10 19 목 SQL

---
+++ Algorithm/Programmers/MySQL/Level3/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,11 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/77487
+
+SELECT ID, NAME, HOST_ID
+FROM PLACES
+WHERE HOST_ID IN (
+    SELECT HOST_ID
+    FROM PLACES
+    GROUP BY HOST_ID
+    HAVING COUNT(HOST_ID) >= 2
+)
+ORDER BY ID
